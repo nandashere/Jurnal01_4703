41132fb794288e2e1c27c6e5b23df7f6d967975b
unknown <anandamartiza@student.telkomuniversity.ac.id>
2023-09-28 13:31:07 +0700

    Jurnal01

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main (String[] args) {
+        System.out.println("Nama: Ananda Martiza Febrina");
+        System.out.println("NIM: 607062330128");
+        System.out.println("Kelas: D3IF-47-03");
+    }
+}
